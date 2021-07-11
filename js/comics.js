5da480edec14b1a88e0868e4b5746abfd64d0946
Jiayin <c170124@e.ntu.edu.sg>
2021-07-11 18:52:00 +0800

    feat: Add comics navigation
    To navigate previous/random/next and to display per page

--- js/comics.js
+++ js/comics.js
@@ -1,22 +1,65 @@
 const comicsContainer = document.querySelector("#comics-container");
+let currentComicNumber = 1;
+const lastComicNumber = 2475;
+// default at 1
+let displayNumber = 1;
 
 function buildComicCardHTML(comicData) {
   return `<div id="comics-card" class="comics-card">
             <h1 id="comics-title">${comicData["title"]}</h1>
             <img id="comics-image" src="${comicData["img"]}"/>
+            <p class="comics-title" >${comicData["num"]}</p>
           </div>`;
 }
 
 async function showComicNumber(number) {
   const newComicCard = document.createElement("div");
-
   const response = await fetch(`https://xkcd.vercel.app/?comic=${number}`);
   const comicData = await response.json();
 
   newComicCard.innerHTML = buildComicCardHTML(comicData);
-
   comicsContainer.appendChild(newComicCard);
 }
-showComicNumber(1);
-showComicNumber(5);
-showComicNumber(100);
+
+async function displayComics(numComicsPerPage) {
+  for (let idx = 0; idx < numComicsPerPage; idx++) {
+    await showComicNumber(currentComicNumber + idx);
+  }
+}
+
+function changeComicsDisplay() {
+  displayNumber = document.querySelector("#display-number");
+  return displayNumber.value;
+}
+
+function comicsPageLogic() {
+  if (currentComicNumber === 0) {
+    currentComicNumber = lastComicNumber;
+  }
+  if (currentComicNumber === 2476) {
+    currentComicNumber = 1;
+  }
+}
+
+function previousPage() {
+  currentComicNumber = Number(currentComicNumber) - Number(displayNumber);
+  updateComicsPage();
+}
+
+function nextPage() {
+  currentComicNumber = Number(currentComicNumber) + Number(displayNumber);
+  updateComicsPage();
+}
+
+function randomPage() {
+  currentComicNumber = Math.floor(Math.random() * lastComicNumber + 1);
+  updateComicsPage();
+}
+
+function updateComicsPage() {
+  comicsContainer.innerHTML = "";
+  displayNumber = changeComicsDisplay();
+  comicsPageLogic();
+  displayComics(displayNumber);
+}
+updateComicsPage();
